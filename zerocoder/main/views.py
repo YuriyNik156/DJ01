7fea920d6e80689f3d8e9dbb0bcf1478ded24a03
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-18 19:49:29 +0300

    update 2 files

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django</h1>")
 
+def new(request):
+    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
